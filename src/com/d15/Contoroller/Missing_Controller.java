1d5495310400d7801e99f30a1b1aae1268052f5f
길한종 <seedhw@gmail.com>
2016-10-05 17:28:18 +0900

    실종신고 서블릿 만드는 중

---
+++ src/com/d15/Contoroller/Missing_Controller.java
@@ -0,0 +1,83 @@
+/*
+ * @Class : Missing_Controller
+ * @Date : 2016.10.5
+ * @Author : 길한종
+ * @Desc : 실종신고 게시판에 사용할 Controller
+ */
+
+package com.d15.Contoroller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.d15.Action.Action;
+import com.d15.Action.ActionForward;
+
+@WebServlet("/*.missing")
+public class Missing_Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public Missing_Controller() {
+        
+    }
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+	
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//1.요청받기
+		String request_uri=request.getRequestURI();
+		String context_path=request.getContextPath();
+		String url_command=request_uri.substring(context_path.length());
+							
+		//인터페이스 추가
+		ActionForward forward=null;
+		Action action=null;
+				
+		//2.판단
+		if(url_command.equals("/BoardWrite.missing")){
+			//신고폼 보여주기
+			forward=new ActionForward();
+			forward.setRedirect(false);
+			forward.setPath("include/boardwrite.jsp");
+			try{
+				forward=action.execute(request, response);
+				System.out.println(request.getAttribute("result"));
+			}catch(Exception e){
+				e.getMessage();
+			}
+		} else if(url_command.equals("/BoardWriteOk.missing")){
+			//글쓰기
+			//BoardDto dto=new BoardDto();
+			//dto.setTitle(request.getParameter("title"));
+			//dto.setTitle(request.getParameter("writer"));
+			//BoardDao dao=new BoardDao();
+			//dao.writeok();
+			//action=new BoardWriteOk();
+		}
+		
+		//3.결과저장
+		//4.뷰 정의
+				
+		//5.forward
+		if(forward!=null){
+			if(forward.isRedirect()){
+				 response.sendRedirect(forward.getPath());
+			} else {
+				RequestDispatcher dis=request.getRequestDispatcher(forward.getPath());
+				dis.forward(request, response);
+			}
+		}
+	}
+}
